81d7718fc611f2f7b9ca33d6be4da7f9de2f0a73
vaishvik12 <www.vaishvikjamalta@gmail.com>
2026-04-27 16:05:58 +0530

    saved our cart to localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,13 @@
-export let cart = [{
-  productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity : 2
-},{
-  productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity : 1
-},
-{
-  productId : "54e0eccd-8f36-462b-b68a-8182611d9add",
-  quantity : 9
-}];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if(!cart){
+cart = [];
+}
+
+
+function saveToStorage(){
+  localStorage.setItem("cart",JSON.stringify(cart));
+}
 
 export function addToCart(productId,quantity){
 let matchingItem;
@@ -27,8 +26,9 @@ if (matchingItem) {
     quantity
   });
 };
-};
 
+saveToStorage();
+};
 
 export function removeFromCart(productId){
   const newCart = [];
@@ -40,4 +40,5 @@ export function removeFromCart(productId){
   })
 
   cart = newCart;
+  saveToStorage();
 }
